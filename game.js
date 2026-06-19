c9e7617345d8226ef966f90dba0008ac09e2a5a5
Pasandul Weerasinghe <77296561+RPWeerasinghe@users.noreply.github.com>
2026-06-19 09:06:21 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -1,19 +1,30 @@
 const movieInput = document.getElementById('movieInput');
 const searchBtn = document.getElementById('searchBtn');
+const resultsDiv = document.getElementById('results');
 
 searchBtn.addEventListener('click',function(){
     const movieInfo = movieInput.value;
     console.log("Movie Info:", movieInfo);
 
+    const safeMovieInfo = encodeURIComponent(movieInfo);
     
-const url = `https://www.omdbapi.com/?apikey=YOUR_API_KEY_HERE&t=${movieName}`;;
+const url = `https://api.tvmaze.com/singlesearch/shows?q=${safeMovieInfo}`;
 
-    fetch(apiKey)
+    fetch(url)
     .then(function(response){
         return response.json();
     })
 
     .then(function(data){
         console.log("API Response:", data);
+       resultsDiv.innerHTML = `
+            <img src="${data.image.medium}" alt="Movie Poster">
+        <div class="info-content">
+            <h2>${data.name}</h2>
+            <p><strong>Premiered:</strong> ${data.premiered}</p>
+            <p><strong>Genres:</strong> ${data.genres.join(', ')}</p>
+             <p><strong>Summary:</strong> ${data.summary}</p>
+        </div>
+`;
     })
 })
